211ed15dbc705ad3d2909583b54e8d1ebff497f1
David Jurišić <david.jurisic@roxoft.hr>
2021-12-22 15:57:43 +0100

    Added html report functionality

--- src/test/java/RunAeviAdminTests.java
+++ src/test/java/RunAeviAdminTests.java
@@ -1,15 +1,51 @@
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import net.masterthought.cucumber.presentation.PresentationMode;
+import net.masterthought.cucumber.sorting.SortingMethod;
+import org.junit.AfterClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resources/features/"},
         strict = false,
         plugin = {"pretty",
-                    "json:target/cucumber_json_reports/report.json",
-                    "html:target/cucumber_html_reports/"},
+                    "json:target/cucumber_json_reports/report.json"},
         glue = {"hook.UI","glue.UI"},
         monochrome = false)
+
 public class RunAeviAdminTests {
+    @SuppressWarnings("unchecked")
+    @AfterClass
+    public static void generateReport() throws IOException {
+        File reportOutputDirectory = new File("target/cucumber_html_reports");
+        List<String> jsonFiles = new ArrayList<>();
+        jsonFiles.add("target/cucumber_json_reports/report.json");
+
+        String buildNumber = "1";
+        String projectName = "AeviAdminTest";
+        Configuration configuration = new Configuration(reportOutputDirectory, projectName);
+        configuration.setBuildNumber(buildNumber);
+
+        configuration.addClassifications("Browser", "Chrome");
+        configuration.addClassifications("Branch", "release/1.0");
+        configuration.setSortingMethod(SortingMethod.NATURAL);
+        configuration.addPresentationModes(PresentationMode.EXPAND_ALL_STEPS);
+        configuration.addPresentationModes(PresentationMode.PARALLEL_TESTING);
+        configuration.setQualifier("sample", "Chrome 80, mobile");
+        configuration.setTrendsStatsFile(new File("target/test-classes/demo-trends.json"));
+
+        ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, configuration);
+        reportBuilder.generateReports();
+    }
 }
