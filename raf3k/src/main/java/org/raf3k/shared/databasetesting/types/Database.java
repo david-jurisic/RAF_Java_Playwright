001e64ef84c46f1670723ca173e426a144b46f79
David Jurišić <david.jurisic@roxoft.hr>
2022-03-25 14:46:24 +0100

    Added Database and RAFDataTable classes

--- raf3k/src/main/java/org/raf3k/shared/databasetesting/types/Database.java
+++ raf3k/src/main/java/org/raf3k/shared/databasetesting/types/Database.java
@@ -1,39 +1,22 @@
 package org.raf3k.shared.databasetesting.types;
 
+import org.raf3k.apitesting.APIReferences;
 import org.raf3k.shared.ControlObject;
 import org.raf3k.shared.SharedReferences;
-import org.raf3k.shared.SharedVariables;
-import org.raf3k.shared.databasetesting.librarytypes.LibDataTable;
 import org.raf3k.shared.logging.Success;
-import java.io.File;
-import java.sql.*;
+
+import java.sql.Connection;
 import java.text.MessageFormat;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Database extends ControlObject {
 
     private final String connectionString;
-    private final String username;
-    private final String password;
     public Connection connection = null;
-    public RAFDataTable rafDataTable = null;
-    public int iRowsAffected = 0;
 
-    /**
-     * A constructor for the Database class.
-     *
-     * @param sConnectionString connection string for the database, written in JDBC format
-     *                          (e.g. 'jdbc:mysql://address:port/database').
-     *                          For more info on connection string formats,
-     *                          read the JDBC documentation.
-     *                          (https://docs.oracle.com/javase/tutorial/jdbc/basics/connecting.html)
-     * @param sUsername         Database username.
-     * @param sPassword         Database password.
-     * @param sAlias            Database alias.
-     */
-    public Database(String sConnectionString, String sUsername, String sPassword, String sAlias) {
+    public Database(String sConnectionString, String sAlias) {
         String sControl = this.getClass().toString();
         this.sControlType = sControl.substring(sControl.lastIndexOf((".")) + 1);
         this.sAlias = MessageFormat.format("({0})", sAlias);
@@ -44,19 +27,17 @@ public class Database extends ControlObject {
             this.sPath = SharedReferences.hlpr().cleanupPath(Thread.currentThread().getStackTrace()[2].getClassName());
 
         this.connectionString = sConnectionString;
-        this.username = sUsername;
-        this.password = sPassword;
     }
 
     /**
      * Method generates a table from a dictionary hash map.
      *
-     * @param sNameHeader  Name of a table header.
+     * @param sNameHeader Name of a table header.
      * @param sValueHeader Value of a table header.
-     * @param dictionary   Map object
+     * @param dictionary Map object
      * @return Table in HTML format.
      */
-    private String generateTableFromDict(String sNameHeader, String sValueHeader, Map<String, Object> dictionary) {
+    public String generateTableFromDict(String sNameHeader, String sValueHeader, Map<String, Object> dictionary) {
         if (dictionary == null) return "";
         var trs = dictionary.entrySet()
                 .stream()
@@ -68,112 +49,14 @@ public class Database extends ControlObject {
         return "<table><tr><th>" + sNameHeader + "</th><th>" + sValueHeader + "</th></tr>" + tableContents + "</table>";
     }
 
-    /**
-     * Method executes an SQL query from a string.
-     *
-     * @param sQuery        SQL query.
-     * @param statementType SQL Statement type. Use 'retrieval' for actions like 'SELECT' and
-     *                      'manipulation' for actions that alter tables or add something to them like 'ALTER, CREATE, INSERT, etc'.
-     * @return Success object, RAFDataTable, iRowsAffected.
-     */
-    public Success executeQuery(String sQuery, statementType statementType) {
+    public Success executeQuery(String sQuery) {
         Success suc = new Success(this);
-        this.rafDataTable = null;
-        this.iRowsAffected = -1;
 
         try {
-            String sMessageAddon = "";
-            if (sQuery != null && !sQuery.isEmpty()) {
-                sMessageAddon += "<h3>SQL Query:</h3> <br><p>" + sQuery + "</p><br>";
-            }
-            suc.sMessageAddon = sMessageAddon;
 
-            switch (statementType) {
-                case retrieval:
-                    try {
-                        connection = DriverManager.getConnection(connectionString, username, password);
-                        Statement stmt = connection.createStatement();
-                        ResultSet rs = stmt.executeQuery(sQuery);
-
-                        this.rafDataTable = new RAFDataTable(new LibDataTable(rs), "rafDataTable");
-                    } catch (SQLException e) {
-                        throw new RuntimeException(e.getMessage());
-                    }
-                case manipulation:
-                    try {
-                        connection = DriverManager.getConnection(connectionString, username, password);
-                        Statement stmt = connection.createStatement();
-                        Boolean rs = stmt.execute(sQuery);
-                        iRowsAffected = stmt.getUpdateCount();
-                    } catch (SQLException e) {
-                        throw new RuntimeException(e.getMessage());
-                    }
-            }
-            return suc.finish(null);
-        } catch (Exception ex) {
-            return suc.finish(ex);
+        } catch (Exception e) {
+            return suc.finish(e);
         }
-    }
-
-    /**
-     * Method executes an SQL query from a file.
-     *
-     * @param sSqlFileName  Name of an SQL script file.
-     * @param statementType SQL Statement type. Use 'retrieval' for actions like 'SELECT' and
-     *                      'manipulation' for actions that alter tables or add something to them like 'ALTER, CREATE, INSERT, etc'.
-     * @param dicParameters SQL script parameters.
-     * @return Success object, RAFDataTable.
-     */
-    public Success executeFromFile(String sSqlFileName, statementType statementType, java.util.Map<String, Object> dicParameters) {
-        Success suc = new Success(this);
-        this.rafDataTable = null;
-        try {
-            String sqlScriptFilesPath = SharedVariables.configuration.getProperty("sqlScriptFilesPath");
-            String sFilePath = sqlScriptFilesPath + "\\" + sSqlFileName;
-            String sQuery = new Scanner(new File(sFilePath)).useDelimiter("\\Z").next();
-
-            String sMessageAddon = "";
-            if (sSqlFileName != null && !sSqlFileName.isEmpty())
-                sMessageAddon += "<h3>SQL File Name:</h3> <br><p>" + sSqlFileName + "</p><br>";
-
-            sMessageAddon += "<h3>SQL File Path:</h3> <br><p>" + sFilePath + "</p><br>";
-            sMessageAddon += "<h3>SQL Script:</h3> <br><p>" + sQuery + "</p><br>";
-
-            if (dicParameters != null)
-                sMessageAddon += "<h3> Stored Procedure Parameters:</h3> <br>" +
-                        generateTableFromDict("Header", "Value", dicParameters);
-
-            suc.sMessageAddon = sMessageAddon;
-
-            switch (statementType) {
-                case retrieval:
-                    try {
-                        connection = DriverManager.getConnection(connectionString, username, password);
-                        Statement stmt = connection.createStatement();
-                        ResultSet rs = stmt.executeQuery(sQuery);
-
-                        this.rafDataTable = new RAFDataTable(new LibDataTable(rs), "rafDataTable");
-                    } catch (SQLException e) {
-                        throw new RuntimeException(e.getMessage());
-                    }
-                case manipulation:
-                    try {
-                        connection = DriverManager.getConnection(connectionString, username, password);
-                        Statement stmt = connection.createStatement();
-                        Boolean rs = stmt.execute(sQuery);
-                        iRowsAffected = stmt.getUpdateCount();
-                    } catch (SQLException e) {
-                        throw new RuntimeException(e.getMessage());
-                    }
-            }
-            return suc.finish(null);
-        } catch (Exception ex) {
-            return suc.finish(ex);
-        }
-    }
-
-    public enum statementType {
-        manipulation,
-        retrieval
+        return suc;
     }
 }
--- raf3k/src/main/java/org/raf3k/shared/databasetesting/types/RAFDataTable.java
+++ raf3k/src/main/java/org/raf3k/shared/databasetesting/types/RAFDataTable.java
@@ -1,10 +1,9 @@
 package org.raf3k.shared.databasetesting.types;
 
+import db.DataRow;
+import db.DataTable;
 import org.raf3k.shared.ControlObject;
 import org.raf3k.shared.SharedReferences;
-import org.raf3k.shared.databasetesting.librarytypes.LibDataRow;
-import org.raf3k.shared.databasetesting.librarytypes.LibDataTable;
-import org.raf3k.shared.enumerations.Operations;
 import org.raf3k.shared.logging.Success;
 
 import java.sql.SQLException;
@@ -14,10 +13,9 @@ import java.util.List;
 
 public class RAFDataTable extends ControlObject {
 
-    LibDataTable dtTable;
-    public int iRowCount;
+    DataTable dtTable;
 
-    public RAFDataTable(LibDataTable table, String sAlias) {
+    public RAFDataTable(DataTable table, String sAlias) {
         String sControl = this.getClass().toString();
         this.sControlType = sControl.substring(sControl.lastIndexOf((".")) + 1);
         this.sAlias = MessageFormat.format("({0})", sAlias);
@@ -34,21 +32,22 @@ public class RAFDataTable extends ControlObject {
      * Method verifies if table contains value.
      *
      * @param sColumnName Column name where value is.
-     * @param sValue      Value to be verified.
-     * @param bExists     'true' check that the value exists, 'false' checks that a value doesn't exist
+     * @param sValue Value to be verified.
+     * @param bExists 'true' check that the value exists, 'false' checks that a value doesn't exist
      * @return Success object.
      */
-    public Success verifyTableValue(String sColumnName, String sValue, Boolean bExists) {
+    public Success VerifyTableValue(String sColumnName, String sValue, Boolean bExists)
+    {
         return SharedReferences.eval().evaluate(() -> {
             int iRowCount = dtTable.Rows.size();
             List<String> rows = new ArrayList<String>(iRowCount);
 
-            for (LibDataRow row : dtTable.Rows) {
+            for (DataRow row : dtTable.Rows) {
                 try {
                     String columnName = row.Get(sColumnName).toString();
                     rows.add(columnName);
                 } catch (SQLException e) {
-                    throw new RuntimeException(e.getMessage());
+                    e.printStackTrace();
                 }
             }
 
@@ -60,102 +59,4 @@ public class RAFDataTable extends ControlObject {
 
         }, this, "");
     }
-
-    /**
-     * Method verifies if a table column exists.
-     *
-     * @param sColumnName Name of the column to be verified.
-     * @param bExists     Set this to 'false' if you want to check that column does not exist.
-     * @return Success object.
-     */
-    public Success verifyColumnExists(String sColumnName, Boolean bExists) {
-        return SharedReferences.eval().evaluate(() -> {
-            if (bExists && !dtTable.ColumnNames.contains(sColumnName))
-                throw new RuntimeException(MessageFormat.format("Column '{0}' does not exist.", sColumnName));
-
-            if (!bExists && dtTable.ColumnNames.contains(sColumnName))
-                throw new RuntimeException(MessageFormat.format("Column '{0}' exists.", sColumnName));
-
-        }, this, "");
-    }
-
-    /**
-     * Method returns the number of table rows.
-     *
-     * @return Success object, iRowCount.
-     */
-    public Success getRowCount() {
-        return SharedReferences.eval().evaluate(() -> {
-            this.iRowCount = -1;
-
-            try {
-                iRowCount = dtTable.Rows.size();
-            } catch (Exception e) {
-                throw new RuntimeException(e.getMessage());
-            }
-
-        }, this, "");
-    }
-
-    /**
-     * Method returns the number of table rows.
-     *
-     * @param iExpectedRowCount Expected number of rows.
-     * @param operation         Verifies that table has equal/less/more rows than specified
-     * @return Success object.
-     */
-    public Success verifyRowCount(int iExpectedRowCount, Operations operation) {
-        return SharedReferences.eval().evaluate(() -> {
-            int iRowCount = dtTable.Rows.size();
-
-            switch (operation) {
-                case Equals:
-                    if (iRowCount != iExpectedRowCount)
-                        throw new RuntimeException(MessageFormat
-                                .format("Row count not as expected. <br>Expected: {0} <br> Current: {1}",
-                                        iExpectedRowCount, iRowCount));
-                    break;
-                case LessThan:
-                    if (iRowCount >= iExpectedRowCount)
-                        throw new RuntimeException(MessageFormat
-                                .format("Row count not as expected. <br>Expected: {0} <br> Current: {1}",
-                                        iExpectedRowCount, iRowCount));
-                    break;
-                case MoreThan:
-                    if (iRowCount <= iExpectedRowCount)
-                        throw new RuntimeException(MessageFormat
-                                .format("Row count not as expected. <br>Expected: {0} <br> Current: {1}",
-                                        iExpectedRowCount, iRowCount));
-                    break;
-            }
-        }, this, "");
-    }
-
-    /**
-     * Method verifies if a specified table is empty.
-     *
-     * @param bEmpty Set to 'false' if you want to check that the table is empty.
-     * @return Success object.
-     */
-    public Success verifyEmpty(Boolean bEmpty) {
-        return SharedReferences.eval().evaluate(() -> {
-            int iRowCount = dtTable.Rows.size();
-            List<String> rows = new ArrayList<String>(iRowCount);
-
-            for (LibDataRow row : dtTable.Rows) {
-                rows.add(row.toString());
-            }
-
-            if (bEmpty) {
-                if (rows.size() > 0)
-                    throw new RuntimeException(MessageFormat
-                            .format("Table is not empty. Number of rows counted: {0}", rows.size()));
-            }
-            if (!bEmpty) {
-                if (rows.size() == 0)
-                    throw new RuntimeException("Table is empty.");
-            }
-        }, this, "");
-    }
-
 }
