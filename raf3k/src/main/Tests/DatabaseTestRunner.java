84f3def4ed00c55c2438fbc32ed75991d9971ef7
David Jurišić <david.jurisic@roxoft.hr>
2022-03-28 14:54:52 +0200

    Added a method for executing SQL queries to Database.java Added verifyColumnExists(), getRowCount(), verifyRowCount() and verifyEmpty() methods to RAFDataTable.java

--- raf3k/src/main/Tests/DatabaseTestRunner.java
+++ raf3k/src/main/Tests/DatabaseTestRunner.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 import org.raf3k.apitesting.basetypes.APITestCase;
+import org.raf3k.guittesting.webtesting.basetypes.UITestCase;
 import org.raf3k.shared.databasetesting.types.Database;
 import org.raf3k.shared.databasetesting.types.RAFDataTable;
 import org.raf3k.shared.enumerations.Operations;
@@ -16,47 +17,16 @@ public class DatabaseTestRunner extends APITestCase {
 
         String sQuery = "CREATE TABLE MyGuests ( id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, firstname VARCHAR(30) NOT NULL, lastname VARCHAR(30) NOT NULL, email VARCHAR(50) )";
         String sQuery2 = "SELECT * FROM MyGuests";
-        String sQuery3 = "INSERT INTO MyGuests (firstname, lastname, email) VALUES ('David', 'Jurišić', 'david.jurisic@roxoft.hr')";
-        String sQuery4 = "DELETE FROM MyGuests WHERE email='david.jurisic@roxoft.hr'";
 
-        newStep(1, "Execute an INSERT SQL query from a string");
-        success(database.executeQuery(sQuery3, Database.statementType.manipulation));
-        int iRowsAffected = database.iRowsAffected;
-        System.out.println(iRowsAffected);
-
-        newStep(2, "Execute a SELECT SQL query from a string");
+        newStep(1, "Execute a database query from string");
         success(database.executeQuery(sQuery2, Database.statementType.retrieval));
         RAFDataTable rafDataTable = database.rafDataTable;
 
         success(rafDataTable.verifyColumnExists("id", true));
         success(rafDataTable.getRowCount());
         int iRowCount = rafDataTable.iRowCount;
-        System.out.println(iRowCount);
+
         success(rafDataTable.verifyRowCount(iRowCount, Operations.Equals));
-
-        newStep(3, "Execute a DELETE SQL query from a string");
-        success(database.executeQuery(sQuery4, Database.statementType.manipulation));
-        iRowsAffected = database.iRowsAffected;
-        System.out.println(iRowsAffected);
-
-        newStep(4, "Execute a SELECT SQL query and verify that the table is empty");
-        success(database.executeQuery(sQuery2, Database.statementType.retrieval));
-        rafDataTable = database.rafDataTable;
-        success(rafDataTable.verifyEmpty(true));
-
-        newStep(5, "Execute an INSERT SQL query from a file");
-        success(database.executeFromFile("insertScript.sql", Database.statementType.manipulation, null));
-        iRowsAffected = database.iRowsAffected;
-        System.out.println(iRowsAffected);
-
-        newStep(6, "Execute a DELETE SQL query from a file");
-        success(database.executeFromFile("deleteScript.sql", Database.statementType.manipulation, null));
-        iRowsAffected = database.iRowsAffected;
-        System.out.println(iRowsAffected);
-
-        newStep(7, "Execute a SELECT SQL query from a file and verify that the table is empty");
-        success(database.executeFromFile("selectScript.sql", Database.statementType.retrieval, null));
-        rafDataTable = database.rafDataTable;
         success(rafDataTable.verifyEmpty(true));
     }
 }
